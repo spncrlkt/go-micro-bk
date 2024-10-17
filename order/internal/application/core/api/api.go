ef22fa1702e433ad3f675af3b0ea4bed3d9ccaf5
Spencer Liechty <spencerliechty@gmail.com>
2024-10-17 11:14:46 -0600

    Add api.go functions

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -1,6 +1,24 @@
 package api
 
 import (
-    "github.com/spncrlkt/go-micro-bk/go-micro-bk/order/internal/application/core/api
+    "github.com/spncrlkt/go-micro-bk/go-micro-bk/order/internal/application/core/domain"
+    "github.com/spncrlkt/go-micro-bk/go-micro-bk/order/internal/ports"
 )
 
+type Application struct {
+    db ports.DBPort
+}
+
+func NewApplication(db ports.DBPort) *Application {
+    return &Application{
+        db: db,
+    }
+}
+
+func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
+    err := a.db.Save(&order)
+    if err != nil {
+        return domain.Order{}, err
+    }
+    return order, nil
+}
