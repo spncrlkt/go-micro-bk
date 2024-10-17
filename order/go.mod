3d4773a647592ec2f3ecbd3ec08efd76c6ffc28d
Spencer Liechty <spencerliechty@gmail.com>
2024-10-17 11:00:43 -0600

    Initial go-micro-bk commit

---
+++ order/go.mod
@@ -0,0 +1,3 @@
+module github.com/spncrlkt/go-micro-bk/order
+
+go 1.22.7
---
+++ order/internal/application/core/api/api.go
@@ -0,0 +1,6 @@
+package api
+
+import (
+    "github.com/spncrlkt/go-micro-bk/go-micro-bk/order/internal/application/core/api
+)
+
