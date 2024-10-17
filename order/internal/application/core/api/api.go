7ac38e9e788f2ff52948876535fe7d199733a6bc
Spencer Liechty <spencerliechty@gmail.com>
2024-10-17 11:17:50 -0600

    Fmt

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -1,24 +1,24 @@
 package api
 
 import (
-    "github.com/spncrlkt/go-micro-bk/go-micro-bk/order/internal/application/core/domain"
-    "github.com/spncrlkt/go-micro-bk/go-micro-bk/order/internal/ports"
+	"github.com/spncrlkt/go-micro-bk/go-micro-bk/order/internal/application/core/domain"
+	"github.com/spncrlkt/go-micro-bk/go-micro-bk/order/internal/ports"
 )
 
 type Application struct {
-    db ports.DBPort
+	db ports.DBPort
 }
 
 func NewApplication(db ports.DBPort) *Application {
-    return &Application{
-        db: db,
-    }
+	return &Application{
+		db: db,
+	}
 }
 
 func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
-    err := a.db.Save(&order)
-    if err != nil {
-        return domain.Order{}, err
-    }
-    return order, nil
+	err := a.db.Save(&order)
+	if err != nil {
+		return domain.Order{}, err
+	}
+	return order, nil
 }
